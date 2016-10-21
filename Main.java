4055ec03c103b5a4ed5a604d1adddd7c4a13b418
18PatZ <18patz@students.harker.org>
2016-10-21 07:51:45 -0700

    Removed WKSHP TWOOW material

--- Main.java
+++ Main.java
@@ -12,12 +12,10 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.event.EventType;
 
-public class Main extends Application implements EventHandler<KeyEvent> {
+public class Main extends Application {
 
 	private Canvas canvas;
 	private GraphicsContext context;
-	private AnimationTimer timer;
-	private int frameNumber = 0;
 	
 	private static int WINDOW_WIDTH = 800;
 	private static int WINDOW_HEIGHT = 600;
@@ -31,59 +29,11 @@ public class Main extends Application implements EventHandler<KeyEvent> {
 		canvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 		context = canvas.getGraphicsContext2D();
 		
-		timer = new AnimationTimer() {
-			@Override
-			public void handle(long now) {
-				nextFrame();
-			}	
-		};
-		timer.start();
-		
 		Scene scene = new Scene(new Group(canvas));
-		scene.setOnKeyPressed(this);
-		scene.setOnKeyReleased(this);
-		
+		stage.setWidth(WINDOW_WIDTH);
+		stage.setHeight(WINDOW_HEIGHT);
 		stage.setScene(scene);
 		stage.setResizable(false);
 		stage.show();
-	}
-	
-	public void nextFrame() {
-		updateGameState();
-		renderFrame();
-	}
-	
-	public void updateGameState() {
-		frameNumber++;
-	}
-	
-	public void renderFrame() {
-		context.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-		context.setFill(Color.BLACK);
-		context.fillText(
-				"Hello, frame " + frameNumber,
-				WINDOW_WIDTH / 2,
-				WINDOW_HEIGHT / 2
-				);
-	}
-	
-	@Override
-	public void handle(KeyEvent event) {
-		EventType<KeyEvent> eventType = event.getEventType();
-		String key = event.getCode().toString();
-		if (eventType == KeyEvent.KEY_PRESSED) {
-			keyPressed(key);
-		}
-		else if (eventType == KeyEvent.KEY_RELEASED) {
-			keyReleased(key);
-		}
-	}
-	
-	public void keyPressed(String key) {
-		System.out.println(key + " pressed");
-	}
-	
-	public void keyReleased(String key) {
-		System.out.println(key + " released");
 	}
 }
